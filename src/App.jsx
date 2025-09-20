ef1ba0e38a759763491ad65d4d5809bde8d84e05
newbie-saimur <saimurrahmanarnob@gmail.com>
2025-09-21 01:01:29 +0600

    Installation of all necessary resources

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h2>Hello world</h2>
+      <button className='btn btn-primary'>hahaha</button>
+    </>
+  )
+}
+
+export default App
